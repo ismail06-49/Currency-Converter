7ac1c7570a82033f53d77571cbf706939a6bdb8c
ismail haddad <haddadismail59@gmail.com>
2024-12-05 12:51:21 -0500

    add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+
+
+export default function Hero() {
+
+    return (
+        <div className="section">
+            <div className="hero">
+                <h1 className="title"></h1>
+                <p className="description">
+
+                </p>
+            </div>
+            <form >
+                <input type="numper" />
+                <select id="from" >
+                    
+                </select>
+                <select id="to">
+                    
+                </select>
+            </form>
+        </div>
+    )
+}
